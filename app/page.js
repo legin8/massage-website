f107c88d915edc654743716b83f8dacf2e092d31
legin8 <maynne1@student.op.ac.nz>
2023-12-11 10:39:32 +1300

    build: changed register to apply

--- app/page.js
+++ app/page.js
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <main className={page}>
       <div>
-        <Link href="/register" className={linkButton}>Register</Link>
+        <Link href="/massage-apply" className={linkButton}>Register</Link>
       </div>
       <div className={headBlock}>
         <h1 className={pageTitle}>Massage Matters</h1>
